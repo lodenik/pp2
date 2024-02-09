52ba6cc47ef8bf66b67dd8cdc163399037aee04a
lodenik <n.kurapoff@gmail.com>
2024-02-09 21:34:33 +0600

    lab1

---
+++ lab1/data types.py
@@ -0,0 +1,30 @@
+'''
+x=5
+print(type(x))
+#Output: <class 'int'>
+
+
+x="Hello World"
+print(type(x))
+#Output: <class 'str'>
+
+x=20.5
+print(type(x))
+#Output: <class 'float'>
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Output: <class 'list'>
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Output: <class 'tuple'>
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Output: <class 'dict'>
+
+x = True
+print(type(x))
+#Output: <class 'bool'>
+'''
---
+++ lab1/syntax.py
@@ -0,0 +1,22 @@
+'''
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+x = 5
+y = "Hello, World!"
+
+
+print("Hello World")
+
+
+if 5 > 2:
+    print("Yes")
+
+'''
