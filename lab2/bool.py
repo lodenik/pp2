3bb6b1fc60d85b0e747a399b24672287660b30ba
lodenik <n.kurapoff@gmail.com>
2024-02-09 14:01:26 +0600

    lab 2

---
+++ lab2/bool.py
@@ -0,0 +1,20 @@
+'''
+print (10>9)
+#Ouput - "True"
+
+
+print (10 == 9)
+#Output - "False"
+
+
+print(10 < 9)
+#Output - "False"
+
+
+print(bool("abc"))
+#Output - "True"
+
+
+print(bool(0))
+#Output - "False"
+'''
